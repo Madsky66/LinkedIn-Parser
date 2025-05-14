6454330df046d0c73388ceec28fd01e0eacf6001
Madsky66 <pmbussy66@gmail.com>
2025-05-14 18:03:12 +0200

    Enhance Google Sheets functionality: add empty row initialization, improve header management, and streamline data appending logic

--- src/main/kotlin/utils/BotController.kt
+++ src/main/kotlin/utils/BotController.kt
@@ -11,13 +11,14 @@ class BotController {
     val robot = Robot()
     suspend fun detect(clipboard: Clipboard, onNewClipboardContent: (String) -> Unit): Boolean {
         var attempts = 1
-        var clipboardContent = copyUrlContent(robot, clipboard)
+        var clipboardContent = copyPageContent(robot, clipboard)
         var isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
         while (!isProfileDetected && attempts < 100) {
             attempts++
             gC.consoleMessage.value = ConsoleMessage("⏳ Détection de la page en cours... [Tentative : $attempts]", ConsoleMessageType.INFO)
+            openCoordsDialog(robot, clipboard)
             delay(100)
-            clipboardContent = copyUrlContent(robot, clipboard)
+            clipboardContent = copyPageContent(robot, clipboard)
             isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
         }
         if (!isProfileDetected) {gC.consoleMessage.value = ConsoleMessage("❌ La page de profil n'a pas été détectée après $attempts tentatives", ConsoleMessageType.ERROR); return false}
@@ -35,7 +36,7 @@ class BotController {
         keyRelease(KeyEvent.VK_CONTROL)    // <------------------------------------------- Touche "Ctrl" relâchée
     }
 
-    suspend fun copyUrlContent(robot: Robot, clipboard: Clipboard): String {
+    suspend fun copyPageContent(robot: Robot, clipboard: Clipboard): String {
         robot.ctrlAnd(KeyEvent.VK_A)    // <---------------------------------------------- "Ctrl" + "A"
         delay(50)
         robot.ctrlAnd(KeyEvent.VK_C)    // <---------------------------------------------- "Ctrl" + "C"
@@ -44,12 +45,26 @@ class BotController {
         return clipboardContent.toString()
     }
 
+    suspend fun openCoordsDialog(robot: Robot, clipboard: Clipboard): String {
+        robot.ctrlAnd(KeyEvent.VK_L)    // <---------------------------------------------- "Ctrl" + "L"
+        delay(50)
+        robot.ctrlAnd(KeyEvent.VK_C)    // <---------------------------------------------- "Ctrl" + "C"
+        delay(50)
+        var clipboardContent = try {clipboard.getData(DataFlavor.stringFlavor) as? String ?: ""} catch (e: Exception) {println("Erreur : $e")}
+        if (clipboardContent != "") clipboardContent = "${clipboardContent}overlay/contact-info/"
+        robot.ctrlAnd(KeyEvent.VK_V)    // <---------------------------------------------- "Ctrl" + "V"
+        delay(50)
+        robot.keyPress(KeyEvent.VK_ENTER)    // <----------------------------------------- "Enter"
+        delay(50)
+        return clipboardContent.toString()
+    }
+
     fun changeApp() {
         robot.keyPress(KeyEvent.VK_ALT)    // <------------------------------------------------- Touche "Alt" pressée
         robot.delay(10)
-        robot.keyPress(KeyEvent.VK_TAB)    // <------------------------------------------------------------- Touche secondaire pressée
+        robot.keyPress(KeyEvent.VK_TAB)    // <------------------------------------------------- Touche secondaire pressée
         robot.delay(10)
-        robot.keyRelease(KeyEvent.VK_TAB)    // <----------------------------------------------------------- Touche secondaire relâchée
+        robot.keyRelease(KeyEvent.VK_TAB)    // <----------------------------------------------- Touche secondaire relâchée
         robot.delay(10)
         robot.keyRelease(KeyEvent.VK_ALT)    // <----------------------------------------------- Touche "Alt" relâchée
         robot.delay(10)
