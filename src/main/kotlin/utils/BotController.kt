3a1d596bf13865d829c34cf0499f0f928e98c39a
Madsky66 <pmbussy66@gmail.com>
2025-05-14 18:19:43 +0200

    Refactor BotController: improve clipboard handling, enhance profile detection logic, and streamline key press operations

--- src/main/kotlin/utils/BotController.kt
+++ src/main/kotlin/utils/BotController.kt
@@ -8,55 +8,78 @@ import java.awt.event.KeyEvent
 import config.GlobalInstance.config as gC
 
 class BotController {
-    val robot = Robot()
+    private val robot = Robot()
     suspend fun detect(clipboard: Clipboard, onNewClipboardContent: (String) -> Unit): Boolean {
         var attempts = 1
-        var clipboardContent = copyPageContent(robot, clipboard)
+        var clipboardContent = copyPageContent(clipboard)
         var isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
         while (!isProfileDetected && attempts < 100) {
             attempts++
             gC.consoleMessage.value = ConsoleMessage("⏳ Détection de la page en cours... [Tentative : $attempts]", ConsoleMessageType.INFO)
-            openCoordsDialog(robot, clipboard)
-            delay(100)
-            clipboardContent = copyPageContent(robot, clipboard)
+            openCoordsDialog(clipboard)
+            delay(1000)
+            clipboardContent = copyPageContent(clipboard)
             isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
+            if (!isProfileDetected) {
+                delay(1000)
+                clipboardContent = copyPageContent(clipboard)
+                isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
+                if (!isProfileDetected) {
+                    delay(3000)
+                    clipboardContent = copyPageContent(clipboard)
+                    isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
+                    if (!isProfileDetected) {
+                        delay(5000)
+                        clipboardContent = copyPageContent(clipboard)
+                        isProfileDetected = clipboardContent.lines().take(5).any {it.contains("dialogue")} && clipboardContent.length > 5000
+                    }
+                    else {break}
+                }
+                else {break}
+            }
+            else {break}
         }
         if (!isProfileDetected) {gC.consoleMessage.value = ConsoleMessage("❌ La page de profil n'a pas été détectée après $attempts tentatives", ConsoleMessageType.ERROR); return false}
         onNewClipboardContent(clipboardContent)
         return true
     }
 
-    fun Robot.ctrlAnd(key: Int) {
-        keyPress(KeyEvent.VK_CONTROL)    // <--------------------------------------------- Touche "Ctrl" pressée
+    private suspend fun ctrlAnd(key: Int) {
+        robot.keyPress(KeyEvent.VK_CONTROL)    // <---------------------------------------------- "Ctrl" + "C"
+        robot.keyPress(key)    // <------------------------------------------------------------- Touche secondaire pressée
         delay(10)
-        keyPress(key)    // <------------------------------------------------------------- Touche secondaire pressée
+        robot.keyRelease(key)    // <----------------------------------------------------------- Touche secondaire relâchée
+        robot.keyRelease(KeyEvent.VK_CONTROL)    // <------------------------------------------- Touche "Ctrl" relâchée
         delay(10)
-        keyRelease(key)    // <----------------------------------------------------------- Touche secondaire relâchée
-        delay(10)
-        keyRelease(KeyEvent.VK_CONTROL)    // <------------------------------------------- Touche "Ctrl" relâchée
     }
 
-    suspend fun copyPageContent(robot: Robot, clipboard: Clipboard): String {
-        robot.ctrlAnd(KeyEvent.VK_A)    // <---------------------------------------------- "Ctrl" + "A"
+    private suspend fun copyPageContent(clipboard: Clipboard): String {
+        ctrlAnd(KeyEvent.VK_A)    // <---------------------------------------------- "Ctrl" + "A"
         delay(50)
-        robot.ctrlAnd(KeyEvent.VK_C)    // <---------------------------------------------- "Ctrl" + "C"
+        ctrlAnd(KeyEvent.VK_C)    // <---------------------------------------------- "Ctrl" + "C"
         delay(50)
-        val clipboardContent = try {clipboard.getData(DataFlavor.stringFlavor) as? String ?: ""} catch (e: Exception) {println("Erreur : $e")}
-        return clipboardContent.toString()
+        return try {clipboard.getData(DataFlavor.stringFlavor) as? String ?: ""} catch (e: Exception) {println("Erreur : $e"); ""}
     }
 
-    suspend fun openCoordsDialog(robot: Robot, clipboard: Clipboard): String {
-        robot.ctrlAnd(KeyEvent.VK_L)    // <---------------------------------------------- "Ctrl" + "L"
+    private suspend fun openCoordsDialog(clipboard: Clipboard): String {
+        ctrlAnd(KeyEvent.VK_L)    // <---------------------------------------------- "Ctrl" + "L"
         delay(50)
-        robot.ctrlAnd(KeyEvent.VK_C)    // <---------------------------------------------- "Ctrl" + "C"
+        ctrlAnd(KeyEvent.VK_C)    // <---------------------------------------------- "Ctrl" + "C"
         delay(50)
-        var clipboardContent = try {clipboard.getData(DataFlavor.stringFlavor) as? String ?: ""} catch (e: Exception) {println("Erreur : $e")}
-        if (clipboardContent != "") clipboardContent = "${clipboardContent}overlay/contact-info/"
-        robot.ctrlAnd(KeyEvent.VK_V)    // <---------------------------------------------- "Ctrl" + "V"
+
+        val baseUrl = try {clipboard.getData(DataFlavor.stringFlavor) as? String ?: ""}
+        catch (e: Exception) {println("Erreur de récupération d'URL : $e"); ""}
+        val newUrl = if (baseUrl.isNotBlank() && !baseUrl.contains("overlay/contact-info/")) {if (baseUrl.endsWith("/")) baseUrl + "overlay/contact-info/" else "$baseUrl/overlay/contact-info/"} else url
+        val clipboardContent = java.awt.Toolkit.getDefaultToolkit().systemClipboard
+        val stringSelection = java.awt.datatransfer.StringSelection(newUrl)
+        clipboardContent.setContents(stringSelection, null)
+
+        ctrlAnd(KeyEvent.VK_V)    // <---------------------------------------------- "Ctrl" + "V"
         delay(50)
-        robot.keyPress(KeyEvent.VK_ENTER)    // <----------------------------------------- "Enter"
+        robot.keyPress(KeyEvent.VK_ENTER)      // <--------------------------------- "Enter"
+        robot.keyRelease(KeyEvent.VK_ENTER)    // <--------------------------------- "Enter"
         delay(50)
-        return clipboardContent.toString()
+        return newUrl
     }
 
     fun changeApp() {
